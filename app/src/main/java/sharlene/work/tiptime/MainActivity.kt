ad9c04a491b44e082e5d5c79721f6ee2f5ce93c4
Crackmindedsk <shakumbhar@gmail.com>
2021-05-25 00:50:59 +0530

    Modified

--- app/src/main/java/sharlene/work/tiptime/MainActivity.kt
+++ app/src/main/java/sharlene/work/tiptime/MainActivity.kt
@@ -12,5 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+    fun calculateTip(){
+        val stringInTextField=binding.costOfService.text.toString()
+        val cost=stringInTextField.toDouble()
+
+        val tipPercentage=when(binding.tipOption.checkedRadioButtonId){
+            R.id.option_twenty_percent->0.20
+            R.id.option_eighteen_percent->0.18
+            else->0.15
+        }
+        var tip=tipPercentage*cost
+
+        val roundUp=binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip=kotlin.math.ceil(tip)
+        }
+
+        val formattedTip=java.text.NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text=getString(R.string.tip_amount,formattedTip)
+
     }
 }
