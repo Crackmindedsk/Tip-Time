9d237550b66a37331817a53e47ff1420faee40ca
Crackmindedsk <shakumbhar@gmail.com>
2021-05-28 21:31:19 +0530

    Updated UI

--- app/src/main/java/sharlene/work/tiptime/MainActivity.kt
+++ app/src/main/java/sharlene/work/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import sharlene.work.tiptime.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
