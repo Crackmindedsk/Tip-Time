8cf43908a1a306c87ebf2c279f7d0e1a1802fd75
Crackmindedsk <shakumbhar@gmail.com>
2021-05-31 16:36:21 +0530

    Added Total Bill amount addition

--- app/src/main/java/sharlene/work/tiptime/MainActivity.kt
+++ app/src/main/java/sharlene/work/tiptime/MainActivity.kt
@@ -32,30 +32,59 @@ class MainActivity : AppCompatActivity() {
         val stringInTextField = binding.costOfServiceText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
 
+        val stringInBill = binding.billAmountText?.text.toString()
+        val bill = stringInBill.toDoubleOrNull()
+        var amount=bill
+        var tip:Double
         val tipPercentage = when (binding.tipOption.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-        if (cost == null || cost == 0.0) {
-            displayTip(0.0)
+
+
+
+        if((bill==0.0 || bill==null) && (cost==0.0 ||cost==null)){
+            displayTip(0.0,0.0)
+            return
+        }else if (cost == null || cost == 0.0) {
+            if (binding.roundUpSwitch.isChecked) {
+                amount = amount?.let { kotlin.math.ceil(it) }
+            }
+            displayTip(0.0,amount)
+            return
+        }else if(bill == null || bill == 0.0) {
+            tip = tipPercentage * cost
+            if (binding.roundUpSwitch.isChecked) {
+                tip = kotlin.math.ceil(tip)
+            }
+            displayTip(tip,0.0)
             return
         }
-        var tip = tipPercentage * cost
+
+        tip = tipPercentage * cost
+        amount = bill + tip
 
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
+            amount = kotlin.math.ceil(amount)
         }
-        displayTip(tip)
+        displayTip(tip,amount)
+
+
+
     }
 
-    private fun displayTip(tip: Double) {
+    private fun displayTip(tip: Double, amount: Double?) {
         val formattedTip = java.text.NumberFormat.getCurrencyInstance().format(tip)
 
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        val formattedAmount = java.text.NumberFormat.getCurrencyInstance().format(amount)
+
+        binding.totalAmount?.text = getString(R.string.total_amount, formattedAmount)
     }
 
-    fun handleKeyEvent(view: View, keycode: Int): Boolean {
+    private fun handleKeyEvent(view: View, keycode: Int): Boolean {
         if (keycode == KeyEvent.KEYCODE_ENTER) {
             //hide the keyboard
             val inputMethodManager =
