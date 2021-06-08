ec725fee84f2c7ef801b4478cb711e22fc9ca452
abGhost.cyber <xplendo@gmail.com>
2021-06-08 12:11:51 +0800

    changed typeface to product sans, refactored the tip calc

--- app/src/main/java/sharlene/work/tiptime/MainActivity.kt
+++ app/src/main/java/sharlene/work/tiptime/MainActivity.kt
@@ -2,11 +2,16 @@ package sharlene.work.tiptime
 
 import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import sharlene.work.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat.getCurrencyInstance
+import kotlin.math.ceil
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,12 +34,19 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     private fun calculateTip() {
         val stringInTextField = binding.costOfServiceText.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-
         val stringInBill = binding.billAmountText.text.toString()
-        val bill = stringInBill.toDoubleOrNull()
+
+        if (TextUtils.isEmpty(stringInBill) || TextUtils.isEmpty(stringInTextField)) {
+            Toast.makeText(this, "some fields are empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val cost = stringInTextField.toDouble()
+        val bill = stringInBill.toDouble()
+
         var amount = bill
         var tip: Double
         val tipPercentage = when (binding.tipOption.checkedRadioButtonId) {
@@ -45,41 +57,37 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        if ((bill == 0.0 || bill == null) && (cost == 0.0 || cost == null)) {
+        if ((bill == 0.0) && (cost == 0.0)) {
             displayTip(0.0, 0.0)
-            return
-        } else if (cost == null || cost == 0.0) {
+        } else if (cost == 0.0) {
             if (binding.roundUpSwitch.isChecked) {
-                amount = amount?.let { kotlin.math.ceil(it) }
+                amount = ceil(amount)
             }
             displayTip(0.0, amount)
-            return
-        } else if (bill == null || bill == 0.0) {
+        } else if (bill == 0.0) {
             tip = tipPercentage * cost
             if (binding.roundUpSwitch.isChecked) {
-                tip = kotlin.math.ceil(tip)
+                tip = ceil(tip)
             }
             displayTip(tip, tip)
-            return
         }
 
         tip = tipPercentage * cost
         amount = bill + tip
 
         if (binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
-            amount = kotlin.math.ceil(amount)
+            tip = ceil(tip)
+            amount = ceil(amount)
         }
         displayTip(tip, amount)
-
-
     }
 
     private fun displayTip(tip: Double, amount: Double?) {
-        val formattedTip = java.text.NumberFormat.getCurrencyInstance().format(tip)
+        val formattedTip = getCurrencyInstance().format(tip)
 
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-        val formattedAmount = java.text.NumberFormat.getCurrencyInstance().format(amount)
+
+        val formattedAmount = getCurrencyInstance().format(amount)
 
         binding.totalAmount.text = getString(R.string.total_amount, formattedAmount)
     }
